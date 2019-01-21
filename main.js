ac54f224fda26d6ef23d55873777ab7e337b37da
Raffaele Pizzari <raffaele.pizzari@xbav.de>
2019-01-21 01:27:23 +0100

    Add first draft

---
+++ main.js
@@ -0,0 +1,29 @@
+const shell = require('shelljs');
+const fs = require('fs');
+const pipe = require('pipe-functions');
+
+// Get args
+const { argv } = require('yargs')
+  .alias('src', 's')
+  .describe('src', 'The source folder of your VueJS files.')
+  .alias('output', 'o')
+  .describe('output', 'The file where you want to write the output.')
+  .alias('plugin', 'p')
+  .default('plugin', '$t')
+  .describe('plugin', 'The VueJS i18n plugin object')
+  .demand(['src', 'output', 'plugin']);
+
+const lib = require('./lib');
+
+const { src, output } = argv;
+
+shell.rm('-f', output);
+
+pipe(
+  lib.readFilesContent(`${src}/**/*.vue`),
+  lib.mergeFilesContent,
+  lib.extractLocs,
+  lib.normalizeStrings,
+  lib.transformToObject,
+  console.log,
+);
