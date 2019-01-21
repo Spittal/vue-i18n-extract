df21a8615ecf232183c7363b9b763f93914febfc
Raffaele Pizzari <raffaele.pizzari@xbav.de>
2019-01-21 01:27:23 +0100

    Add first draft

---
+++ lib.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const glob = require('glob');
+const dot = require('dot-object');
+
+module.exports = {
+  readFilesContent(src) {
+    const targetFiles = glob.sync(src);
+    return targetFiles.map(f => Object.assign({}, { name: f, content: fs.readFileSync(f, 'utf8') }));
+  },
+
+  mergeFilesContent(files) {
+    let content;
+    files.forEach((f) => {
+      content += f.content;
+    });
+    return content;
+  },
+
+  extractLocs(content) {
+    const regex = /(\$t\(')(.*)('\))/gi;
+    return content.match(regex);
+  },
+
+  normalizeStrings(stringsArray) {
+    return stringsArray.map(s => s.substring(s.lastIndexOf('$t(\'') + 4, s.lastIndexOf('\')')));
+  },
+
+  transformToObject(stringsArray) {
+    const obj = {};
+    stringsArray.forEach((el) => {
+      obj[el] = el;
+    });
+    return dot.object(obj);
+  },
+};
