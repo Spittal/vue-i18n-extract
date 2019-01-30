56e61d452f35b5717afeede6590d5e3ca8277b37
Raffaele Pizzari <raffaele.pizzari@gmail.com>
2019-01-30 11:17:14 +0100

    Add Jest suite

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+      'airbnb-base',
+      'plugin:jest/recommended',
+  ],
+  plugins: [
+      'import',
+      'jest',
+  ],
+  env: {
+      node: true,
+      'jest/globals': true,
+  },
+};
