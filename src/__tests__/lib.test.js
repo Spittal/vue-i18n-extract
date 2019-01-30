8ee5a223cdf3277551c316763693860537105408
Raffaele Pizzari <raffaele.pizzari@gmail.com>
2019-01-30 23:29:55 +0100

    #9 src and lang must be a glob pattern

--- src/__tests__/lib.test.js
+++ src/__tests__/lib.test.js
@@ -1,23 +1,45 @@
+/* eslint-disable */
+require = require('esm')(module);
+/* eslint-disable */
 const lib = require('../lib');
 
-
 describe('Lib', () => {
-  describe('getFilesContent', () => {
+  describe('readVueFiles', () => {
     test('get vue and js files from src', () => {
       const src = './src/__tests__/test_demo_files/**/*.?(js|vue)';
-      const results = lib.getFilesContent(src);
+      const results = lib.readVueFiles(src);
       expect(results.length).toEqual(4);
     });
     test('doesnt find any file', () => {
       const src = './src/__tests__/test_demo_files/**/*.?(txt)';
-      const results = lib.getFilesContent(src);
+      const results = lib.readVueFiles(src);
       expect(results.length).toEqual(0);
     });
     test('get vue and js files and read the content', () => {
       const src = './src/__tests__/test_demo_files/**/*.?(js|vue)';
-      const results = lib.getFilesContent(src);
+      const results = lib.readVueFiles(src);
       results.forEach((r) => {
         expect(r.name.split('/')[r.name.split('/').length - 1]).toEqual(r.content);
+      });
+    });
+  });
+
+  describe('readLangFiles', () => {
+    test('get vue and js files from src', () => {
+      const src = './src/__tests__/test_lang_files/*.js';
+      const results = lib.readLangFiles(src);
+      expect(results.length).toEqual(2);
+    });
+    test('doesnt find any file', () => {
+      const src = './src/__tests__/test_lang_files/*.?(txt)';
+      const results = lib.readLangFiles(src);
+      expect(results.length).toEqual(0);
+    });
+    test('get vue and js files and read the content', () => {
+      const src = './src/__tests__/test_lang_files/*.js';
+      const results = lib.readLangFiles(src);
+      results.forEach((r) => {
+        expect(typeof r.content.header).toEqual('object');
       });
     });
   });
