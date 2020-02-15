986dc0f239f65be5b4f77e04bcbcf9d9cc3ef423
kleinfreund <philipprudloff@fastmail.com>
2020-02-15 19:19:50 +0100

    add dedicated parsing testcases

--- tests/unit/library/file-operations-vue.spec.ts
+++ tests/unit/library/file-operations-vue.spec.ts
@@ -3,10 +3,70 @@ import { SimpleFile, I18NItem } from '@/library/models';
 import path from 'path';
 
 describe('file-operations-vue.ts', () => {
-  it('Extract supported i18n strings from a collection of file paths', () => {
+  it('extracts supported i18n strings from a collection of file paths', () => {
     const src: string = path.resolve(__dirname, '../fixtures/vue-files/**/*.?(js|vue)');
     const filesCollection: SimpleFile[] = readVueFiles(src);
     const results: I18NItem[] = extractI18nItemsFromVueFiles(filesCollection);
     expect(results.length).toEqual(26);
+  });
+
+  it('extracts translation keys from methods correctly', () => {
+    const testFiles = [
+      {
+        content: `$t('Natural-language key with spaces')`,
+        translationKey: 'Natural-language key with spaces',
+        fileName: '',
+        path: ''
+      },
+      {
+        content: `$t('path.without.spaces')`,
+        translationKey: 'path.without.spaces',
+        fileName: '',
+        path: ''
+      },
+      {
+        content: `$t('Don\\'t leave me behind!')`,
+        translationKey: 'Don\\\'t leave me behind!',
+        fileName: '',
+        path: ''
+      },
+      {
+        content: `$t('Early ' string termination')`,
+        translationKey: 'Early ',
+        fileName: '',
+        path: ''
+      },
+      {
+        content: `$t("Early " string termination")`,
+        translationKey: 'Early ',
+        fileName: '',
+        path: ''
+      },
+      {
+        content: `$t(\`Escaped backtick: \\\`\`)`,
+        translationKey: 'Escaped backtick: \\\`',
+        fileName: '',
+        path: ''
+      },
+      {
+        content: `$t('Nested "quotes"')`,
+        translationKey: 'Nested "quotes"',
+        fileName: '',
+        path: ''
+      },
+      {
+        content: `$t(
+          'Argument on new line'
+        )`,
+        translationKey: 'Argument on new line',
+        fileName: '',
+        path: ''
+      },
+    ]
+
+    const results = extractI18nItemsFromVueFiles(testFiles);
+    for (let i = 0; i < results.length; i++) {
+      expect(testFiles[i].translationKey).toBe(results[i].path)
+    }
   });
 });
