ebca6a99a62b079b85600accad6941370a091c80
kleinfreund <philipprudloff@fastmail.com>
2020-02-15 16:14:25 +0100

    fix extracting strings with escaped quotation marks

--- tests/unit/fixtures/vue-files/file3.vue
+++ tests/unit/fixtures/vue-files/file3.vue
@@ -1,2 +1,6 @@
-const test = $t('Key As Fallback');
-const test = $t('Missing');
+const test1 = $t('Key As Fallback');
+const test2 = $t('Missing');
+const test3 = $t('Don\'t leave me behind!');
+const test4 = $t('Early ' string termination');
+const test5 = $t("Early " string termination");
+const test6 = $t(`Backtick: \``);
