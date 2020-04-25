1aac7f58ef3188fe738dabe3262d7354588467c0
Daniel Small <piktur.io@gmail.com>
2020-04-25 23:04:49 +1000

    ignore case when matching i18n tag

--- tests/unit/fixtures/vue-files/folder/file3.vue
+++ tests/unit/fixtures/vue-files/folder/file3.vue
@@ -3,4 +3,9 @@ const pluralText = tc('missing.c', 5);
 <i18n path="header.paragraphs.p_a">
   <a>{{ $t('missing.d') }}</a>
 </i18n>
-
+<I18n path="header.paragraphs.p_b">
+  <a>{{ $t('missing.e') }}</a>
+</I18n>
+<I18N path="header.paragraphs.p_c">
+  <a>{{ $t('missing.f') }}</a>
+</I18N>
