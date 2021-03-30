eb14317635dea357b0a77dc613e3b3ecc1eab4e2
Dobroslav Totev <dev@totev.de>
2021-03-30 17:19:37 +0200

    fix: invalid js when generating config file

--- src/init-command/index.ts
+++ src/init-command/index.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 const configFile = `
 module.exports = {
   vueFilesPath: './', // The Vue.js file(s) you want to extract i18n strings from. It can be a path to a folder or to a file. It accepts glob patterns. (ex. *, ?, (pattern|pattern|pattern)
-  languageFilesPath: './', The language file(s) you want to compare your Vue.js file(s) to. It can be a path to a folder or to a file. It accepts glob patterns (ex. *, ?, (pattern|pattern|pattern)
+  languageFilesPath: './', // The language file(s) you want to compare your Vue.js file(s) to. It can be a path to a folder or to a file. It accepts glob patterns (ex. *, ?, (pattern|pattern|pattern)
   options: {
     output: false, // false | string => Use if you want to create a json file out of your report. (ex. output.json)
     add: false, // false | true => Use if you want to add missing keys into your json language files.
