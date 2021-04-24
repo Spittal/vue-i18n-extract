c68075bd7a3256525b8e648f4b1a564766801ade
Jamie Spittal <lastmanout@gmail.com>
2021-04-24 15:39:23 -0700

    feat: removing default export, for tree shaking
    BREAKING CHANGE: Any node app that uses this must now use the individual imports from the index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,3 @@ export * from './init-command';
 
 export * from './types';
 export * from './report-command';
-
-// Needed for compatibility with versions < 1.1.0 of vue-i18n-extract.
-export default {
-  ...report
-};
