cda3cb4b22f76f3a3baf0c83d52ef8afe7f5d266
Jamie Spittal <lastmanout@gmail.com>
2021-05-16 13:39:38 -0700

    feat: simplify fixtures

---
+++ tests/fixtures/vue-files/Basic.vue
@@ -0,0 +1,12 @@
+<template functional>
+  <div>
+    <h1>{{ $t('header.title') }}</h1>
+    <p>{{ $t("content.paragraph.p_1", { count: 'two' }) }}</p>
+    <p>{{ $t('Key used as default translation.') }}</p>
+    <p>{{ $tc(`content.paragraph.p.2`, 2) }}</p>
+    <i18n path="content.link.a">
+      <a place="link" href="#">{{ $t('content.link.b') }}</a>
+    </i18n>
+    <p v-t="'header.title'"></p>
+  </div>
+</template>
--- tests/fixtures/vue-files/folder1/Nested.vue
+++ tests/fixtures/vue-files/folder1/Nested.vue
@@ -1,17 +1,3 @@
-<script>
-export default {
-  name: 'test',
-  data () {
-    return {
-      a: this.$t('data.example'),
-    }
-  }
-}
-</script>
-
-<template>
-  <div>
-    <h1>{{ a }}</h1>
-    <p v-t="'header.titles.title_1'"></p>
-  </div>
+<template functional>
+  <h1>{{ $t('header.title') }}</h1>
 </template>
