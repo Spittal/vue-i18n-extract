bbabfa008a7c134a973b7da2e54f10ea348a0a3f
Vince Ricosti <vricosti@outlook.com>
2021-08-31 15:50:42 +0200

    Added an option to override the DotObject separator

--- bin/vue-i18n-extract.js
+++ bin/vue-i18n-extract.js
@@ -31,6 +31,10 @@ cli
     '--ci',
     '[boolean] The process will exit with exitCode=1 if at least one translation-key is missing (useful expecially if it is part of a CI pipeline).',
   )
+  .option(
+    '--separator <separator>',
+    'Use if you want to override the separator used when parsing locale identifiers. Default is `.`'
+   )
   .action((options) => {
     createI18NReport(resolveConfig(options));
   });
